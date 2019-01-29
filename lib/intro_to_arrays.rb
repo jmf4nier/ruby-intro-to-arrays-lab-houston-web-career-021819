a49dc61392a696d44ba7efd3735acf570d6a72b0
Jason Fournier <jmf4nier@hotmail.com>
2019-01-29 16:39:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,15 @@
 def instantiate_new_array
   array =[]
 end
+def array_with_two_elements
+  array = [1,2]
+end
+def first_element(array)
+  array[0]
+end
+def third_element(array)
+  array[2]
+end
+def last_element(array)
+  
+end 
